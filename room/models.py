503dfbccd504b156a7933a995ea0cbac132c6ff3
ndolocr <crndolo@gmail.com>
2022-12-10 21:18:36 +0300

    Creating migrations for room app

--- room/models.py
+++ room/models.py
@@ -21,4 +21,4 @@ class Room(models.Model):
     created_on = models.DateTimeField( auto_now_add = True)
 
     def __str__(self):
-        return self.name
+        return self.room_name + " - " + str(self.house)

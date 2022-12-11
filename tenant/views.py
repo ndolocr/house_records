c1a7519c6f67acdf62a38ca57257c1e55a3e8748
ndolocr <crndolo@gmail.com>
2022-12-11 22:21:27 +0300

    Creating view to see all tenant information

--- tenant/views.py
+++ tenant/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from tenant.models import Tenant
 
 # Create your views here.
+
+def view_all_tenants(request):
+    all_tenants = Tenant.objects.all()
+
+    context = {
+        "all_tenants": all_tenants,
+    }
+
+    return render(request, 'tenant/index.html', context)
